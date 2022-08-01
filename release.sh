4a67534b8ac8ee6cdac84e59d06348520e1cd8bc
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-08-02 02:21:04 +0300

    Release: 1.1.1

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 echo "Creeating release $1"
 git add -A
 git commit --allow-empty -am "Release: $1"
-gh release create $1 --generate-notes
 git pull
 git push
+gh release create $1 --generate-notes
 git tag --list
